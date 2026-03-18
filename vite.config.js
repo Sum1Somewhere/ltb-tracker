5f70e6c02c736660c97cd84d3b8c2d9ec5b30213
Sum1Somewhere <bryjgardner@gmail.com>
2026-03-17 22:26:42 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    include: ['**/*.js', '**/*.jsx'],
+  })],
   root: 'public',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true,
   }
 })
